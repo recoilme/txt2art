0e96877c49da8e5be68220b0c74d5d153f6e938b
recoilme <vadim-kulibaba@yandex.ru>
2024-10-02 10:47:15 +0300

    sdservice

--- sdservice.py
+++ sdservice.py
@@ -52,8 +52,6 @@ def encode_images_to_base64(images):
     return json.dumps(encoded_images)
 
 def txt2img(prompt1,prompt2):
-    if len(prompt1)>300 and len(prompt2)>100:
-        prompt1=""
     negative_prompt = "worst quality, low quality, text, censored, deformed, bad hand, blurry, watermark, multiple phones, weights, bunny ears, extra hands, extra fingers, deformed fingers"
     prompt_embeds, prompt_neg_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds =  get_weighted_text_embeddings_sdxl(pipe, prompt = prompt1+prompt2, neg_prompt = negative_prompt)
 
