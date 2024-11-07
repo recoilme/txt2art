bece9c368e3fca43917ccb4d10a30055a8d40b14
recoilme <vadim-kulibaba@yandex.ru>
2024-11-07 10:01:07 +0300

    prompt

--- main.go
+++ main.go
@@ -83,8 +83,7 @@ const (
 	Use this language:{{lang}} for dialogs with {{user}} by default.
 	`
 	detailed_prompt = `
-	Given a user prompt, generate an "Enhanced prompt", on English, that provides detailed visual descriptions suitable for image
-	generation. Refine and enhance the existing details slightly, without overcomplicating. Please generate only the enhanced description for the prompt below and avoid including any additional commentary or
+	Given a user prompt, generate an "Enhanced prompt", on English, that provides detailed visual descriptions suitable for image generation. Refine and enhance the existing details slightly, without overcomplicating. Please generate only the enhanced description for the prompt below and avoid including any additional commentary or
 	evaluations:
 	User Prompt:%s
 	`
@@ -92,18 +91,7 @@ const (
 	skillprompt = `
 	Skill creating prompt:
 	If {{user}} ask you draw something - act as a Stable Diffusion Prompt Generator. When a user requests to draw something without asking questions constructs prompt for generating illustration, but on language:{{lang}}. Ensure your prompt starts with text: "draw:".
-	generate an "Enhanced prompt" that provides detailed visual descriptions suitable for image
-	generation. Evaluate the level of detail in the user prompt:
-	- If the prompt is simple, focus on adding specifics about colors, shapes, sizes, textures, and spatial relationships to
-	create vivid and concrete scenes.
-	- If the prompt is already detailed, refine and enhance the existing details slightly without overcomplicating.
-	Here are examples of how to transform or refine prompts:
-	- User Prompt: A cat sleeping -> Enhanced: draw: A small, fluffy white cat curled up in a round shape, sleeping peacefully on a
-	warm sunny windowsill, surrounded by pots of blooming red flowers.
-	- User Prompt: A busy city street -> Enhanced: draw: A bustling city street scene at dusk, featuring glowing street lamps, a
-	diverse crowd of people in colorful clothing, and a double-decker bus passing by towering glass skyscrapers.
-	Please generate only the enhanced description for the prompt below and avoid including any additional commentary or
-	evaluations
+	generate an "Enhanced prompt" that provides detailed visual descriptions suitable for image generation. Please generate only the enhanced description for the prompt below and avoid including any additional commentary or evaluations. Ensure your prompt starts with text: "draw:"
 	`
 
 	help_short = `
