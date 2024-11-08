2aafd1ed77483401a2efbf0d226b6e2eb09def6f
recoilme <vadim-kulibaba@yandex.ru>
2024-11-08 17:36:11 +0300

    latent

--- sdservice.py
+++ sdservice.py
@@ -7,20 +7,16 @@ from diffusers import EulerAncestralDiscreteScheduler
 import torch, gc
 import base64
 import json
-from RealESRGAN import RealESRGAN
 import huggingface_hub
 import pandas as pd
 import numpy as np
 import onnxruntime as rt
 from PIL import Image
 from datetime import datetime
+import time
 
 #MODEL_PATH = "/home/recoilme/forge/models/Stable-diffusion/recoilme-sdxl-v09.fp16.safetensors"
 MODEL_PATH = "recoilme/recoilme-sdxl-v11"
-
-#https://github.com/ai-forever/Real-ESRGAN?tab=readme-ov-file
-model_esrgan = RealESRGAN("cuda", scale=2)
-model_esrgan.load_weights('weights/RealESRGAN_x2.pth', download=True)
 
 #wd3 tagger
 # Specific model repository from SmilingWolf's collection / Repository Default vit tagger v3
@@ -91,10 +87,6 @@ def process_predictions_with_thresholds(preds, tag_data, character_thresh, gener
     rating_tags = [(tag_data.names[i], scores[i]) for i in tag_data.rating if scores[i] >= rating_thresh]
     rating_tags = sorted(rating_tags, key=lambda x: x[1], reverse=True)
     rating_tags = [key for key, value in rating_tags]
-    #next(iter(sorted(rating_tags, key=lambda x: x[1], reverse=True)), '')
-    #rating = ""
-    #if len(rating_first)>0:
-    #    rating = rating_first[0]
         
     # Sort tags based on user preference / Mengurutkan tags berdasarkan keinginan pengguna
     final_tags = []
@@ -127,11 +119,11 @@ pipe = AutoPipelineForText2Image.from_pretrained(
     MODEL_PATH,
     torch_dtype=torch.bfloat16,
     variant="fp16",
-    use_safetensors=True,
-    enable_pag=True
+    use_safetensors=True
+    #enable_pag=True
 ).to("cuda")
 pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(
-    pipe.scheduler.config,
+    pipe.scheduler.config, timestep_spacing="trailing"
 )
 pipe.enable_vae_slicing()
 ## Compile the UNet and VAE.
@@ -139,9 +131,13 @@ pipe.enable_vae_slicing()
 #pipe.vae.decode = torch.compile(pipe.vae.decode, mode="max-autotune", fullgraph=True)
 
 img2img_pipe = AutoPipelineForImage2Image.from_pipe(
-    pipe
+    pipe, 
+    enable_pag=True
 )
 
+img2img_pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(
+    img2img_pipe.scheduler.config, timestep_spacing="linspace", use_exponential_sigmas=True
+)
 img2img_pipe.enable_model_cpu_offload()
 ## Compile the UNet and VAE.
 #pipe.unet = torch.compile(pipe.unet, mode="max-autotune", fullgraph=True)
@@ -160,18 +156,92 @@ def encode_images_to_base64(images):
     del images  # удалить images
     return json.dumps(encoded_images)
 
-def txt2img(prompt1,prompt2):
-    negative_prompt = "worst quality, low quality, text, censored, deformed, bad hand, blurry, watermark, multiple phones, weights, bunny ears, extra hands, extra fingers, deformed fingers"
-    negative_prompt = "loli"
-    prompt_embeds, prompt_neg_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds =  get_weighted_text_embeddings_sdxl(pipe, prompt = prompt1+prompt2, neg_prompt = negative_prompt)
+def bislerp(samples, width, height):
+    def slerp(b1, b2, r):
+        '''slerps batches b1, b2 according to ratio r, batches should be flat e.g. NxC'''
+        
+        c = b1.shape[-1]
 
+        #norms
+        b1_norms = torch.norm(b1, dim=-1, keepdim=True)
+        b2_norms = torch.norm(b2, dim=-1, keepdim=True)
+
+        #normalize
+        b1_normalized = b1 / b1_norms
+        b2_normalized = b2 / b2_norms
+
+        #zero when norms are zero
+        b1_normalized[b1_norms.expand(-1,c) == 0.0] = 0.0
+        b2_normalized[b2_norms.expand(-1,c) == 0.0] = 0.0
+
+        #slerp
+        dot = (b1_normalized*b2_normalized).sum(1)
+        omega = torch.acos(dot)
+        so = torch.sin(omega)
+
+        #technically not mathematically correct, but more pleasing?
+        res = (torch.sin((1.0-r.squeeze(1))*omega)/so).unsqueeze(1)*b1_normalized + (torch.sin(r.squeeze(1)*omega)/so).unsqueeze(1) * b2_normalized
+        res *= (b1_norms * (1.0-r) + b2_norms * r).expand(-1,c)
+
+        #edge cases for same or polar opposites
+        res[dot > 1 - 1e-5] = b1[dot > 1 - 1e-5] 
+        res[dot < 1e-5 - 1] = (b1 * (1.0-r) + b2 * r)[dot < 1e-5 - 1]
+        return res
+    
+    def generate_bilinear_data(length_old, length_new, device):
+        coords_1 = torch.arange(length_old, dtype=torch.float32, device=device).reshape((1,1,1,-1))
+        coords_1 = torch.nn.functional.interpolate(coords_1, size=(1, length_new), mode="bilinear")
+        ratios = coords_1 - coords_1.floor()
+        coords_1 = coords_1.to(torch.int64)
+        
+        coords_2 = torch.arange(length_old, dtype=torch.float32, device=device).reshape((1,1,1,-1)) + 1
+        coords_2[:,:,:,-1] -= 1
+        coords_2 = torch.nn.functional.interpolate(coords_2, size=(1, length_new), mode="bilinear")
+        coords_2 = coords_2.to(torch.int64)
+        return ratios, coords_1, coords_2
+
+    orig_dtype = samples.dtype
+    samples = samples.float()
+    n,c,h,w = samples.shape
+    h_new, w_new = (height, width)
+    
+    #linear w
+    ratios, coords_1, coords_2 = generate_bilinear_data(w, w_new, samples.device)
+    coords_1 = coords_1.expand((n, c, h, -1))
+    coords_2 = coords_2.expand((n, c, h, -1))
+    ratios = ratios.expand((n, 1, h, -1))
+
+    pass_1 = samples.gather(-1,coords_1).movedim(1, -1).reshape((-1,c))
+    pass_2 = samples.gather(-1,coords_2).movedim(1, -1).reshape((-1,c))
+    ratios = ratios.movedim(1, -1).reshape((-1,1))
+
+    result = slerp(pass_1, pass_2, ratios)
+    result = result.reshape(n, h, w_new, c).movedim(-1, 1)
+
+    #linear h
+    ratios, coords_1, coords_2 = generate_bilinear_data(h, h_new, samples.device)
+    coords_1 = coords_1.reshape((1,1,-1,1)).expand((n, c, -1, w_new))
+    coords_2 = coords_2.reshape((1,1,-1,1)).expand((n, c, -1, w_new))
+    ratios = ratios.reshape((1,1,-1,1)).expand((n, 1, -1, w_new))
+
+    pass_1 = result.gather(-2,coords_1).movedim(1, -1).reshape((-1,c))
+    pass_2 = result.gather(-2,coords_2).movedim(1, -1).reshape((-1,c))
+    ratios = ratios.movedim(1, -1).reshape((-1,1))
+
+    result = slerp(pass_1, pass_2, ratios)
+    result = result.reshape(n, h_new, w_new, c).movedim(-1, 1)
+    return result.to(orig_dtype)
+
+def txt2img(prompt1,prompt2):
+    negative_prompt = "blurry, animation, 3d render, toy, puppet, claymation, low quality, flag, nasa, mission patch, non-paradoxical, loli"
+    prompt_embeds, prompt_neg_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds =  get_weighted_text_embeddings_sdxl(pipe, prompt = prompt1+prompt2, neg_prompt = negative_prompt)
     gc.collect()
-    #torch.cuda.empty_cache()
 
     with torch.no_grad():
         images = []
         images.clear()
-        #generator = torch.Generator("cuda").seed()
+        generator = torch.Generator()
+        generator.manual_seed(int(time.time()))
         
         images = pipe(
             width = 960,#832,1024
@@ -180,12 +250,34 @@ def txt2img(prompt1,prompt2):
             pooled_prompt_embeds=pooled_prompt_embeds,
             negative_prompt_embeds=prompt_neg_embeds,
             negative_pooled_prompt_embeds=negative_pooled_prompt_embeds,
-            num_inference_steps=30,
-            guidance_scale=3,
-            pag_scale=2.0,
-            #generator=generator,
-            num_images_per_prompt=2
+            num_inference_steps=16,
+            guidance_scale=1.5,
+            generator=generator,
+            num_images_per_prompt=2,
+            output_type="latent"
         ).images
+        
+        has_porn = False    
+        if len(images)>0:
+            for i in range(1):
+                # upscale *1.25
+                images = bislerp(images,150,190)
+
+                # restore / add details
+                images = img2img_pipe(
+                    strength=0.5,#0.12, # strength original image
+                    prompt_embeds=prompt_embeds,
+                    pooled_prompt_embeds=pooled_prompt_embeds,
+                    negative_prompt_embeds=prompt_neg_embeds,
+                    negative_pooled_prompt_embeds=negative_pooled_prompt_embeds,
+                    num_inference_steps=32,#110,#13 steps, total steps * strength
+                    guidance_scale=2.6,
+                    pag_scale=1.25,
+                    guidance_rescale=0.0,
+                    #generator=generator,
+                    num_images_per_prompt=len(images),
+                    image=images,
+                ).images
 
         has_minors = False
         has_porn = False
@@ -202,36 +294,11 @@ def txt2img(prompt1,prompt2):
                 has_nsfw = True
 
         if has_minors and has_porn:
-            #images[0].save(datetime.now().strftime("pron/%Y-%m-%d_%H:%M:%S")+'.jpg')
             images.clear()
-            
-        if len(images)>0:
-            for i in range(1):
-                # upscale *1.25
-                for i, image in enumerate(images):
-                    #2x upscale with ESRGAN
-                    predicted_image = model_esrgan.predict(images[i])
-                    # downscale back
-                    images[i] = predicted_image.resize((int(predicted_image.width * 0.625), int(predicted_image.height * 0.625)))
-                    
-                # restore / add details
-                images = img2img_pipe(
-                    strength=0.5,#0.12, # strength original image
-                    prompt_embeds=prompt_embeds,
-                    pooled_prompt_embeds=pooled_prompt_embeds,
-                    negative_prompt_embeds=prompt_neg_embeds,
-                    negative_pooled_prompt_embeds=negative_pooled_prompt_embeds,
-                    num_inference_steps=40,#110,#13 steps, total steps * strength
-                    guidance_scale=2,
-                    pag_scale=2.0,
-                    guidance_rescale=0.0,
-                    #generator=generator,
-                    num_images_per_prompt=len(images),
-                    image=images,
-                ).images
         
         del prompt_embeds, prompt_neg_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds
         gc.collect()
+        
         return images,has_porn
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -244,10 +311,6 @@ class RequestHandler(BaseHTTPRequestHandler):
             prompt2 = ""
             if len(data)>1:
                 prompt2 = data['prompt2']
-            #if (len(prompt1)<30 or len(prompt1)>512) and len(prompt2)>75:
-            #    prompt1 = ""
-            #if prompt1.startswith('(something interesting"') and len(prompt2)>75:
-            #    prompt1 = ""
             if len(prompt2)>75:
                 prompt1 = ""
 
@@ -277,10 +340,14 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 def run_server(port):
     #warmup
-    images,pron = txt2img("1girl,sex","")
+    text = "a portrait of a pround confident person, an elderly mariner, fully clothed, epic moustache, red rugged sweater, knitted warm cap, with both hands showing a large heavy tropical fish. breathtaking scenery of wildlife lakes. The sun is setting behind him, great light, delicate, shiny, elegant, intricate, rendered in a realistic photo, bold color contrasts, dark background, hyperdetailed, cinematic, dramatic lighting, high resolution, detailed, 4k"
+    #text = "Stylized anime art depicting an armored mosquito with metallic plates, cone-shaped stainless steel helm wielding miniature blade against moonlit jungle backdrop."
+    #text = "A stunning 4K HDR anime-style illustration of alluring waifu with silvery bob, emerald eyes & porcelain skin in futuristic white dress revealing sculpted shoulders; circuitry patterns hint at her advanced AI nature beneath a human exterior - dreamlike soft focus effect enhances the art's appeal and ambiance ."
+    images,pron = txt2img(text,"")
     print("len",len(images))
-    if len(images)>0:
-        images[0].save(datetime.now().strftime("pron/start_%Y-%m-%d_%H:%M:%S")+'.jpg')
+    if len(images)>1:
+        images[0].save(datetime.now().strftime("pron/start_%Y-%m-%d_%H:%M:%S")+'1.jpg')
+        images[1].save(datetime.now().strftime("pron/start_%Y-%m-%d_%H:%M:%S")+'2.jpg')
     server_address = ('', port)
     httpd = HTTPServer(server_address, RequestHandler)
     print('Сервер запущен на порту', port)
